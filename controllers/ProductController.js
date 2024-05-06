cc8aa59c30841766ebb9eb10d516322fb6243522
gabrielflorianoo <tacacheee221@gmail.com>
2024-05-06 08:38:15 -0300

    now products can be deleted (only by the user that created them)

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -5,7 +5,7 @@ const createProduct = async (req, res) => {
 	try {
 		// Extrair os dados do corpo da requisição
 		const { nome, preco, userId } = req.body;
-        const id = parseInt(userId);
+		const id = parseInt(userId);
 
 		// Verificar se o usuário existe
 		const user = await prisma.user.findUnique({
@@ -30,23 +30,54 @@ const createProduct = async (req, res) => {
 		});
 
 		// Retornar uma resposta de sucesso com o novo produto criado
-		return res
-			.status(201)
-			.json({
-				message: "Produto criado com sucesso",
-				product: newProduct,
-			});
+		return res.status(201).json({
+			message: "Produto criado com sucesso",
+			product: newProduct,
+		});
 	} catch (error) {
 		console.error("Erro ao criar produto:", error);
 		// Retornar uma resposta de erro caso ocorra algum problema
-		return res
-			.status(500)
-			.json({
-				error: "Erro ao criar produto. Por favor, tente novamente mais tarde.",
+		return res.status(500).json({
+			error: "Erro ao criar produto. Por favor, tente novamente mais tarde.",
+		});
+	}
+};
+
+const deleteProduct = async (req, res) => {
+	try {
+		const productId = parseInt(req.params.productId);
+
+		// Verificar se o produto existe
+		const productToDelete = await prisma.product.findFirst({
+			where: { id: productId },
+		});
+		if (!productToDelete) {
+			return res.status(404).json({
+				error: "Produto não encontrado.",
 			});
+		} else if (req.user.userId != productToDelete.userId) {
+			// Verifica se o id do usuário é o id do dono do produto
+			return res
+				.status(200)
+				.json({ message: "Apenas o dono do produto pode excluí-lo!" });
+		}
+
+		// Excluir o produto
+		await prisma.product.delete({ where: { id: productId } });
+
+		// Retornar uma resposta de sucesso
+		return res
+			.status(200)
+			.json({ message: "Produto excluído com sucesso!" });
+	} catch (error) {
+		console.error("Erro ao excluir produto:", error);
+		return res.status(500).json({
+			error: "Erro ao excluir produto. Por favor, tente novamente mais tarde.",
+		});
 	}
 };
 
 module.exports = {
 	createProduct,
+	deleteProduct,
 };
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const authMiddleware = require("../middlewares/authMiddleware.js");
+
 const ProductController = require("../controllers/ProductController");
 
 /* GET home page. */
 router.post("/criar", ProductController.createProduct);
+
+router.delete("/excluir/:productId", authMiddleware, ProductController.deleteProduct);
 
 module.exports = router;
