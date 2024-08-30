ec00b737a453fb0f99a6c2e3cd2185bf6cc9e969
gabrielflo <88804052+gabrielflorianoo@users.noreply.github.com>
2024-08-29 21:54:11 -0300

    Commit pós apresentação

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const adminMiddleware = require("../middlewares/admin.js");
+
+const { Product, Order } = require('../Sequelize/Models.js');
+
 // Importar o controller de usuários
 const UserController = require("../controllers/UserController");
 
@@ -18,5 +22,25 @@ router.put("/atualizar", authMiddleware, UserController.updateUser);
 
 // Rota para deletar um usuário
 router.delete("/deletar", authMiddleware, UserController.deleteUser);
+
+router.get('/:id', adminMiddleware, async (req, res, next) => {
+    const products = await Product.findAll({
+        where: {
+            userId: req.params.id,
+        }
+    });
+    const orders = await Order.findAll({
+        where: {
+            userId: req.params.id,
+        }
+    });
+
+    console.log(products.length);
+
+    res.send({
+        produtos: products.length,
+        pedidos: orders.length
+    });
+});
 
 module.exports = router;
